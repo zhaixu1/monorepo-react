607e4003e3fcd76ab0d6b61a5fe9c110117e5d5d
zhaixu1 <49115132+zhaixu1@users.noreply.github.com>
2025-07-22 17:02:30 +0000

    Deploying to gh-pages from @ zhaixu1/monorepo-react@76c6dcb7524dc6d1d41efa70679d1b6ac08e2246 🚀

---
+++ meta__packages.ce24128e.async.js
@@ -0,0 +1,163 @@
+"use strict";(self.webpackChunkmonorepo=self.webpackChunkmonorepo||[]).push([[56],{24827:function(r,n,e){var t;e.r(n),e.d(n,{demos:function(){return u}});var l=e(90228),i=e.n(l),I=e(87999),c=e.n(I),_=e(39546),v=e(42025),o=e(94813),u={"button-demo-demo":{component:_.memo(_.lazy(function(){return e.e(570).then(e.bind(e,93761))})),asset:{type:"BLOCK",id:"button-demo-demo",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:e(40070).Z},react:{type:"NPM",value:"19.1.0"},"../index.tsx":{type:"FILE",value:e(45857).Z},"./index.less":{type:"FILE",value:e(24519).Z}},entry:"index.tsx"},context:{"../index.tsx":v,"./index.less":o,react:t||(t=e.t(_,2)),"/home/runner/work/monorepo-react/monorepo-react/packages/components/src/Button/index.tsx":v,"/home/runner/work/monorepo-react/monorepo-react/packages/components/src/Button/index.less":o},renderOpts:{compile:function(){var d=c()(i()().mark(function s(){var p,f=arguments;return i()().wrap(function(x){for(;;)switch(x.prev=x.next){case 0:return x.next=2,e.e(319).then(e.bind(e,72319));case 2:return x.abrupt("return",(p=x.sent).default.apply(p,f));case 3:case"end":return x.stop()}},s)}));function m(){return d.apply(this,arguments)}return m}()}}}},92855:function(r,n,e){var t;e.r(n),e.d(n,{demos:function(){return m}});var l=e(90228),i=e.n(l),I=e(87999),c=e.n(I),_=e(39546),v=e(53782),o=e(95703),u=e(91302),d=e(32327),m={"textellipsis-demo-demo":{component:_.memo(_.lazy(function(){return e.e(612).then(e.bind(e,95802))})),asset:{type:"BLOCK",id:"textellipsis-demo-demo",refAtomIds:["TextEllipsis"],dependencies:{"index.tsx":{type:"FILE",value:e(45374).Z},react:{type:"NPM",value:"19.1.0"},"../index.tsx":{type:"FILE",value:e(20906).Z},antd:{type:"NPM",value:"5.26.6"},"@monorepo/lib":{type:"NPM",value:"1.0.0"},"./index.less":{type:"FILE",value:e(63316).Z}},entry:"index.tsx"},context:{"../index.tsx":v,"./index.less":d,react:t||(t=e.t(_,2)),"/home/runner/work/monorepo-react/monorepo-react/packages/components/src/TextEllipsis/index.tsx":v,antd:o,"@monorepo/lib":u,"/home/runner/work/monorepo-react/monorepo-react/packages/components/src/TextEllipsis/index.less":d},renderOpts:{compile:function(){var s=c()(i()().mark(function f(){var a,x=arguments;return i()().wrap(function(h){for(;;)switch(h.prev=h.next){case 0:return h.next=2,e.e(319).then(e.bind(e,72319));case 2:return h.abrupt("return",(a=h.sent).default.apply(a,x));case 3:case"end":return h.stop()}},f)}));function p(){return s.apply(this,arguments)}return p}()}}}},43770:function(r,n,e){e.r(n),e.d(n,{demos:function(){return l}});var t=e(39546),l={}},96655:function(r,n,e){e.r(n),e.d(n,{demos:function(){return l}});var t=e(39546),l={}},1594:function(r,n,e){var t;e.r(n),e.d(n,{demos:function(){return o}});var l=e(90228),i=e.n(l),I=e(87999),c=e.n(I),_=e(39546),v=e(48839),o={"packages-hooks-src-use-focus-demo-demo":{component:_.memo(_.lazy(function(){return e.e(433).then(e.bind(e,32174))})),asset:{type:"BLOCK",id:"packages-hooks-src-use-focus-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(83372).Z},react:{type:"NPM",value:"19.1.0"},"zxreact-hooks":{type:"NPM",value:"1.0.7"}},entry:"index.tsx"},context:{react:t||(t=e.t(_,2)),"zxreact-hooks":v},renderOpts:{compile:function(){var u=c()(i()().mark(function m(){var s,p=arguments;return i()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(319).then(e.bind(e,72319));case 2:return a.abrupt("return",(s=a.sent).default.apply(s,p));case 3:case"end":return a.stop()}},m)}));function d(){return u.apply(this,arguments)}return d}()}}}},59887:function(r,n,e){e.r(n),e.d(n,{demos:function(){return l}});var t=e(39546),l={}},79451:function(r,n,e){e.r(n),e.d(n,{demos:function(){return l}});var t=e(39546),l={}},5117:function(r,n,e){var t;e.r(n),e.d(n,{demos:function(){return o}});var l=e(90228),i=e.n(l),I=e(87999),c=e.n(I),_=e(39546),v=e(48839),o={"packages-hooks-src-use-window-size-demo-demo":{component:_.memo(_.lazy(function(){return e.e(433).then(e.bind(e,5563))})),asset:{type:"BLOCK",id:"packages-hooks-src-use-window-size-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(83700).Z},react:{type:"NPM",value:"19.1.0"},"zxreact-hooks":{type:"NPM",value:"1.0.7"}},entry:"index.tsx"},context:{react:t||(t=e.t(_,2)),"zxreact-hooks":v},renderOpts:{compile:function(){var u=c()(i()().mark(function m(){var s,p=arguments;return i()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(319).then(e.bind(e,72319));case 2:return a.abrupt("return",(s=a.sent).default.apply(s,p));case 3:case"end":return a.stop()}},m)}));function d(){return u.apply(this,arguments)}return d}()}}}},42025:function(r,n,e){e.r(n);var t=e(26068),l=e.n(t),i=e(67825),I=e.n(i),c=e(39546),_=e(94813),v=e(74132),o=["children"],u=function(m){var s=m.children,p=I()(m,o);return(0,v.jsx)("button",l()(l()({className:"my-btn"},p),{},{children:s}))};n.default=u},53782:function(r,n,e){e.r(n);var t=e(48305),l=e.n(t),i=e(39546),I=e(31156),c=e(91302),_=e(32327),v=e(74132);function o(u){console.log((0,c.isUndef)(23232),(0,c.isUndef)(null),"isUndef");var d=u.content,m=(0,i.useState)(!1),s=l()(m,2),p=s[0],f=s[1],a=(0,i.useRef)(null),x=(0,i.useMemo)(function(){return(0,c.isUndef)(d)?"------":d},[d]);return(0,i.useEffect)(function(){if(a!=null&&a.current){var P=window.getComputedStyle(a.current).fontSize,h=window.getComputedStyle(a.current).width.replace("px",""),E=document.createElement("p");E.style.fontSize=P,E.style.whiteSpace="nowrap",E.style.position="fixed",E.style.top="-100px",E.style.opacity="0",E.innerHTML=d,document.body.appendChild(E);var g=document.createRange();g.setStart(E,0),g.setEnd(E,E.childNodes.length);var M=g.getBoundingClientRect().width;document.body.removeChild(E),E=null,console.log(h,"width"),console.log(M,"textWidth"),h&&M&&parseInt(h)<M?(console.log(111),f(!0)):f(!1)}},[d]),(0,v.jsx)("div",{className:"mt-ui-ellipsis",ref:a,children:p?(0,v.jsx)(I.Z,{title:x,placement:"topLeft",children:(0,v.jsx)("span",{className:"mt-ui-ellipsis",children:x})}):(0,v.jsx)("span",{className:"screen-min",children:x})})}n.default=(0,i.memo)(o)},94813:function(r,n,e){e.r(n)},32327:function(r,n,e){e.r(n)},91285:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[]},53923:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[]},34352:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"\u8FD9\u91CC\u662F ",paraId:0,tocIndex:0},{value:"Monorepo-React",paraId:0,tocIndex:0},{value:" \u9879\u76EE\u7684\u7EC4\u4EF6\u5E93\uFF0C\u5305\u542B\u4E86\u4E00\u7CFB\u5217\u9AD8\u8D28\u91CF\u7684 React \u7EC4\u4EF6\u3002",paraId:0,tocIndex:0}]},4981:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"\u8FD9\u91CC\u662F ",paraId:0,tocIndex:0},{value:"Monorepo-React",paraId:0,tocIndex:0},{value:" \u9879\u76EE\u7684 Hooks \u5E93\uFF0C\u5305\u542B\u4E86\u4E00\u7CFB\u5217\u9AD8\u8D28\u91CF\u7684 React Hooks\uFF0C\u5E2E\u52A9\u4F60\u66F4\u9AD8\u6548\u5730\u5F00\u53D1 React \u5E94\u7528\u3002",paraId:0,tocIndex:0}]},41324:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"const [eleRef, setFocusFn] = useFocus()",paraId:0},{value:"\u53C2\u6570",paraId:1,tocIndex:2},{value:"\u8BF4\u660E",paraId:1,tocIndex:2},{value:"\u7C7B\u578B",paraId:1,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:2},{value:"\u65E0",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:` const [eleRef, setFocusFn] = useFocus();
+`,paraId:2,tocIndex:3},{value:"eleRef: \u83B7\u53D6\u7126\u70B9\u7684\u5143\u7D20",paraId:3,tocIndex:3},{value:"setFocusFn: \u8BBE\u7F6E\u7126\u70B9\u7684\u65B9\u6CD5",paraId:3,tocIndex:3}]},45595:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"useStorage",paraId:0,tocIndex:0},{value:" \u662F\u4E00\u4E2A\u7528\u4E8E\u5728 React \u7EC4\u4EF6\u4E2D\u4FBF\u6377\u5730\u8BFB\u5199\u672C\u5730\u5B58\u50A8\uFF08localStorage/sessionStorage\uFF09\u7684\u81EA\u5B9A\u4E49 Hook\u3002\u5B83\u8BA9\u4F60\u50CF\u4F7F\u7528 useState \u4E00\u6837\u64CD\u4F5C\u672C\u5730\u5B58\u50A8\u7684\u6570\u636E\uFF0C\u5E76\u4E14\u6570\u636E\u4F1A\u81EA\u52A8\u540C\u6B65\u5230\u5B58\u50A8\u4E2D\u3002",paraId:0,tocIndex:0},{value:"const [value, setValue] = useStorage(key, initialValue?, storage?)",paraId:1},{value:"\u53C2\u6570",paraId:2,tocIndex:3},{value:"\u8BF4\u660E",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:3},{value:"key",paraId:2,tocIndex:3},{value:"\u5B58\u50A8\u7684 key",paraId:2,tocIndex:3},{value:"string",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3},{value:"initialValue",paraId:2,tocIndex:3},{value:"\u521D\u59CB\u503C",paraId:2,tocIndex:3},{value:"any",paraId:2,tocIndex:3},{value:"undefined",paraId:2,tocIndex:3},{value:"storage",paraId:2,tocIndex:3},{value:"\u5B58\u50A8\u7C7B\u578B\uFF0ClocalStorage \u6216 sessionStorage",paraId:2,tocIndex:3},{value:"Storage",paraId:2,tocIndex:3},{value:"window.localStorage",paraId:2,tocIndex:3},{value:"key",paraId:3,tocIndex:3},{value:"\uFF1A\u5FC5\u586B\uFF0C\u5B58\u50A8\u7684\u952E\u540D\u3002",paraId:3,tocIndex:3},{value:"initialValue",paraId:3,tocIndex:3},{value:"\uFF1A\u53EF\u9009\uFF0C\u521D\u59CB\u503C\u3002",paraId:3,tocIndex:3},{value:"storage",paraId:3,tocIndex:3},{value:"\uFF1A\u53EF\u9009\uFF0C\u5B58\u50A8\u7C7B\u578B\uFF0C\u9ED8\u8BA4\u4E3A localStorage\u3002",paraId:3,tocIndex:3},{value:"value",paraId:4,tocIndex:4},{value:"\uFF1A\u5F53\u524D\u5B58\u50A8\u7684\u503C\u3002",paraId:4,tocIndex:4},{value:"setValue",paraId:4,tocIndex:4},{value:"\uFF1A\u8BBE\u7F6E\u503C\u7684\u65B9\u6CD5\uFF0C\u8C03\u7528\u540E\u4F1A\u81EA\u52A8\u540C\u6B65\u5230\u672C\u5730\u5B58\u50A8\u3002",paraId:4,tocIndex:4},{value:`const [value, setValue] = useStorage('my-key', '\u9ED8\u8BA4\u503C');
+setValue('\u65B0\u503C');
+`,paraId:5,tocIndex:6},{value:`const [value, setValue] = useStorage('my-key', '\u9ED8\u8BA4\u503C', window.sessionStorage);
+`,paraId:6,tocIndex:7},{value:`const [user, setUser] = useStorage('user', { name: '\u5F20\u4E09' });
+`,paraId:7,tocIndex:8},{value:"useStorage",paraId:8,tocIndex:9},{value:" \u4F1A\u81EA\u52A8\u5C06\u975E\u5B57\u7B26\u4E32\u7C7B\u578B\u7684\u503C\u7528 JSON \u5E8F\u5217\u5316\u5B58\u50A8\u3002",paraId:8,tocIndex:9},{value:"\u8BFB\u53D6\u65F6\u4F1A\u81EA\u52A8\u53CD\u5E8F\u5217\u5316\u4E3A\u539F\u59CB\u7C7B\u578B\u3002",paraId:8,tocIndex:9},{value:"\u9002\u7528\u4E8E localStorage \u548C sessionStorage\u3002",paraId:8,tocIndex:9}]},58914:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"useToggle",paraId:0,tocIndex:0},{value:" \u662F\u4E00\u4E2A\u7528\u4E8E\u5728\u4E24\u4E2A\u72B6\u6001\u503C\u4E4B\u95F4\u5207\u6362\u7684 React Hook\uFF0C\u652F\u6301\u5E03\u5C14\u503C\u6216\u4EFB\u610F\u81EA\u5B9A\u4E49\u503C\u3002\u5E38\u7528\u4E8E\u5F00\u5173\u3001\u72B6\u6001\u5207\u6362\u7B49\u573A\u666F\u3002",paraId:0,tocIndex:0},{value:"const [state, actions] = useToggle(initialState?, reverseValue?)",paraId:1},{value:"\u53C2\u6570",paraId:2,tocIndex:2},{value:"\u8BF4\u660E",paraId:2,tocIndex:2},{value:"\u7C7B\u578B",paraId:2,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:2},{value:"initialState",paraId:2,tocIndex:2},{value:"\u521D\u59CB\u72B6\u6001",paraId:2,tocIndex:2},{value:"T",paraId:2,tocIndex:2},{value:"false",paraId:2,tocIndex:2},{value:"reverseValue",paraId:2,tocIndex:2},{value:"\u53CD\u8F6C\u72B6\u6001\u503C",paraId:2,tocIndex:2},{value:"U",paraId:2,tocIndex:2},{value:"!T",paraId:2,tocIndex:2},{value:"initialState",paraId:3,tocIndex:2},{value:"\uFF1A\u53EF\u9009\uFF0C\u521D\u59CB\u72B6\u6001\uFF0C\u9ED8\u8BA4\u4E3A ",paraId:3,tocIndex:2},{value:"false",paraId:3,tocIndex:2},{value:"\u3002",paraId:3,tocIndex:2},{value:"reverseValue",paraId:3,tocIndex:2},{value:"\uFF1A\u53EF\u9009\uFF0C\u5207\u6362\u65F6\u7684\u53CD\u8F6C\u503C\uFF0C\u9ED8\u8BA4\u4E3A ",paraId:3,tocIndex:2},{value:"!initialState",paraId:3,tocIndex:2},{value:"\u3002",paraId:3,tocIndex:2},{value:"state",paraId:4,tocIndex:3},{value:"\uFF1A\u5F53\u524D\u72B6\u6001\u503C\u3002",paraId:4,tocIndex:3},{value:"actions",paraId:4,tocIndex:3},{value:"\uFF1A\u5305\u542B\u4EE5\u4E0B\u65B9\u6CD5\u7684\u5BF9\u8C61\uFF1A",paraId:4,tocIndex:3},{value:"\u65B9\u6CD5\u540D",paraId:5,tocIndex:3},{value:"\u8BF4\u660E",paraId:5,tocIndex:3},{value:"\u7C7B\u578B",paraId:5,tocIndex:3},{value:"toggle",paraId:5,tocIndex:3},{value:"\u5207\u6362\u72B6\u6001",paraId:5,tocIndex:3},{value:"() => void",paraId:5,tocIndex:3},{value:"set",paraId:5,tocIndex:3},{value:"\u8BBE\u7F6E\u4E3A\u6307\u5B9A\u503C",paraId:5,tocIndex:3},{value:"(value: T|U) => void",paraId:5,tocIndex:3},{value:"setLeft",paraId:5,tocIndex:3},{value:"\u8BBE\u7F6E\u4E3A\u521D\u59CB\u72B6\u6001",paraId:5,tocIndex:3},{value:"() => void",paraId:5,tocIndex:3},{value:"setRight",paraId:5,tocIndex:3},{value:"\u8BBE\u7F6E\u4E3A\u53CD\u8F6C\u503C",paraId:5,tocIndex:3},{value:"() => void",paraId:5,tocIndex:3},{value:`const [state, { toggle }] = useToggle();
+toggle(); // false -> true -> false ...
+`,paraId:6,tocIndex:6},{value:`const [color, { toggle }] = useToggle('red', 'blue');
+toggle(); // red <-> blue
+`,paraId:7,tocIndex:7}]},23555:function(r,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"useWindowSize  \u5B9E\u65F6\u83B7\u53D6\u5F53\u524D\u7A97\u53E3\u5C3A\u5BF8\u3002",paraId:0,tocIndex:0},{value:"const size = useWindowSize();",paraId:1,tocIndex:1},{value:"\u8FD4\u56DE\u503C",paraId:2,tocIndex:1},{value:"\u8BF4\u660E",paraId:2,tocIndex:1},{value:"\u7C7B\u578B",paraId:2,tocIndex:1},{value:"size",paraId:2,tocIndex:1},{value:"\u7A97\u53E3\u5C3A\u5BF8",paraId:2,tocIndex:1},{value:"{ width: number; height: number; }",paraId:2,tocIndex:1}]},40070:function(r,n){n.Z=`import React from 'react'
+import Button from '../index'
+export default function Index() {
+  return (
+    <div>
+        <Button>\u6309\u94AE</Button>
+    </div>
+  )
+}
+`},24519:function(r,n){n.Z=`.my-btn {
+  padding: 8px 16px;
+  background: hsl(0, 92%, 50%);
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+`},45857:function(r,n){n.Z=`import React from "react";
+import "./index.less";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <button className="my-btn" {...rest}>{children}</button>
+);
+
+export default Button;`},45374:function(r,n){n.Z=`import React from 'react'
+import TextEllipsis from '../index'
+// @ts-ignore
+// import { TextEllipsis } from '../../../dist/index.es.js'
+
+export default function Index() {
+  return (
+    <div style={{width: 200}}>
+        <TextEllipsis content="\u6D4B\u8BD5\u4E00\u6BB5\u5F88\u957F\u7684\u6587\u5B57\u6D4B\u8BD5\u4E00\u6BB5\u5F88\u957F\u7684\u6587\u5B57\u6D4B\u8BD5\u4E00\u6BB5\u5F88\u957F\u7684\u6587\u5B57\u6D4B\u8BD5\u4E00\u6BB5\u5F88\u957F\u7684\u6587\u5B57\u6D4B\u8BD5\u4E00\u6BB5\u5F88\u957F\u7684\u6587\u5B57\u6D4B\u8BD5\u4E00\u6BB5\u5F88\u957F\u7684\u6587\u5B57"></TextEllipsis>
+    </div>
+  )
+}
+`},63316:function(r,n){n.Z=`.mt-ui-ellipsis {
+    display: block;
+    width: 100%;
+}
+
+.mt-ui-ellipsis {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+
+.mt-ui-ellipsis2 {
+    display: -webkit-box !important;
+    overflow: hidden;
+    white-space: normal !important;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+}
+
+.mt-ui-ellipsis3 {
+    display: -webkit-box !important;
+    overflow: hidden;
+    white-space: normal !important;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+}
+`},20906:function(r,n){n.Z=`import React, { useMemo, memo, useRef, useEffect, useState } from 'react';
+import { Tooltip } from 'antd';
+import { isUndef } from '@monorepo/lib';
+import './index.less'
+function TextEllipsis(props: any) {
+    console.log(isUndef(23232), isUndef(null), 'isUndef');
+    
+  const { content } = props;
+  const [isEllipsis, setEllipsis] = useState(false);
+  const wrap = useRef(null);
+
+  const realContent = useMemo(() => {
+    
+    return isUndef(content) ? '------' : content;
+  }, [content]);
+
+  useEffect(() => {
+    if (!wrap?.current) return;
+
+    let fontSize = window.getComputedStyle(wrap.current).fontSize;
+    let width = window.getComputedStyle(wrap.current).width.replace('px', '');
+    let p: HTMLParagraphElement | null = document.createElement('p');
+    p.style.fontSize = fontSize;
+    p.style.whiteSpace = 'nowrap';
+    p.style.position = 'fixed';
+    p.style.top = '-100px';
+    p.style.opacity = '0';
+    p.innerHTML = content;
+    document.body.appendChild(p);
+    const range = document.createRange();
+    range.setStart(p, 0);
+    range.setEnd(p, p.childNodes.length);
+    let textWidth = range.getBoundingClientRect().width;
+    document.body.removeChild(p);
+    p = null;
+    console.log(width, 'width');
+    console.log(textWidth, 'textWidth');
+
+    
+    
+    if (width && textWidth && parseInt(width) < textWidth) {
+        console.log(111);
+        
+      setEllipsis(true);
+    } else {
+      setEllipsis(false);
+    }
+  }, [content]);
+
+  return (
+    <div className={'mt-ui-ellipsis'} ref={wrap}>
+      {isEllipsis ? (
+        <Tooltip title={realContent} placement="topLeft">
+          <span className="mt-ui-ellipsis">{realContent}</span>
+        </Tooltip>
+      ) : (
+        <span className="screen-min">{realContent}</span>
+      )}
+    </div>
+  );
+}
+
+export default memo(TextEllipsis);
+`},83372:function(r,n){n.Z=`import React from "react";
+import { useFocus } from "zxreact-hooks";
+
+export default function Index() {
+    const [focusRef, setFocus] = useFocus();
+    return (
+        <div>
+            <input ref={focusRef} style={{ marginRight: "20px" }} />
+            <button onClick={() => setFocus()}>\u4E8B\u4EF6\u805A\u7126</button>
+        </div>
+    );
+}
+`},83700:function(r,n){n.Z=`import React from "react";
+import { useWindowSize } from "zxreact-hooks";
+export default function index() {
+    const windowSize = useWindowSize();
+    return (
+        <div>
+            \u6D4F\u89C8\u5668\u7A97\u53E3\u5927\u5C0F\uFF1A{windowSize.width} x {windowSize.height}
+        </div>
+    );
+}
+`},48839:function(r,n,e){e.r(n),e.d(n,{useFocus:function(){return _},useStorage:function(){return c},useToggle:function(){return l},useWindowSize:function(){return v}});var t=e(39546);function l(o,u){o===void 0&&(o=!1);var d=(0,t.useState)(o),m=d[0],s=d[1];return[m,(0,t.useMemo)(function(){var p=u===void 0?!o:u;return{toggle:function(){return s(function(a){return a===o?p:o})},set:function(a){return s(a)},setLeft:function(){return s(o)},setRight:function(){return s(p)}}},[])]}function i(o,u){return c(o,u,window.localStorage)}function I(o,u){return c(o,u,window.sessionStorage)}function c(o,u,d){var m=(0,t.useState)(function(){var a=d.getItem(o);return a!=null?JSON.parse(a):typeof u=="function"?u():u}),s=m[0],p=m[1];(0,t.useEffect)(function(){if(s===void 0)return d.removeItem(o);d.setItem(o,JSON.stringify(s))},[o,s,d]);var f=(0,t.useCallback)(function(){p(void 0)},[]);return[s,p,f]}function _(){var o=(0,t.useRef)(null),u=(0,t.useCallback)(function(){var d;(d=o==null?void 0:o.current)===null||d===void 0||d.focus()},[]);return[o,u]}var v=function(){var u=(0,t.useState)({width:window.innerWidth,height:window.innerHeight}),d=u[0],m=u[1];return(0,t.useEffect)(function(){var s=function(){m({width:window.innerWidth,height:window.innerHeight})};return window.addEventListener("resize",s),function(){window.removeEventListener("resize",s)}},[]),d}},91302:function(r,n,e){e.r(n),e.d(n,{add:function(){return t},isUndef:function(){return i},minus:function(){return l}});function t(I,c){return I+c}function l(I,c){return I-c}function i(I){return console.log("is not dist"),I==null}}}]);
